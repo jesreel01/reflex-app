60542adafb0bba5de30cacbe7f229bc9435cdc2c
jesreel01 <jesreelmiole93@gmail.com>
2024-12-02 18:47:25 +0800

    feat: rename project to Reflex; add AI recommendation tab and update tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import { Stack, Tabs } from "expo-router";
 import { ThemeToggle } from "~/components/ThemeToggle";
 import { BottomNav } from "~/components/ui/bottom-nav";
+import Svg, { Path } from "react-native-svg";
+import Feather from "@expo/vector-icons/Feather";
+import { WandSparkles, ScanQrCode } from 'lucide-react-native';
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+
 
 const TabLayout = () => {
   return (
@@ -10,24 +15,52 @@ const TabLayout = () => {
       <Tabs.Screen
         name="index"
         options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            
+            <Feather  name="home" size={20} color={color} />
+          ),
+
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="ai-recommendation"
+        options={{
+          title: "AI Assistant",
+          tabBarIcon: ({ color }) => (
+            
+              <WandSparkles size={20} color={color}/>
+          ),
           headerShown: false,
         }}
       />
       <Tabs.Screen
         name="scan"
         options={{
+          title: "Scan QR",
+          tabBarIcon: ({ color }) => (
+            <ScanQrCode size={24} color={color} />
+          ),
           headerShown: false,
         }}
       />
+
       <Tabs.Screen
         name="wardrobe"
         options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="wardrobe-outline" size={24} color={color} />
+          ),
           headerShown: false,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="user" size={20} color={color} />
+          ),
           headerShown: false,
         }}
       />
---
+++ app/(tabs)/ai-recommendation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AiRecommendation = () => {
+  return (
+    <View>
+      <Text>AiRecommendation</Text>
+    </View>
+  )
+}
+
+export default AiRecommendation
