84db7d3a04ef423ff4372c727e2b8495fb5008f7
Jesreel <jesreelmiole93@gmail.com>
2024-12-06 15:23:56 +0800

    feat: integrate camera functionality with barcode scanning; add camera permissions and flash controls

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import React from "react";
 import { Stack, Tabs } from "expo-router";
 import { ThemeToggle } from "~/components/ThemeToggle";
 import { BottomNav } from "~/components/ui/bottom-nav";
 import Svg, { Path } from "react-native-svg";
 import Feather from "@expo/vector-icons/Feather";
-import { WandSparkles, ScanQrCode } from 'lucide-react-native';
+import { WandSparkles, ScanQrCode, RefreshCwOff } from "lucide-react-native";
 import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
-
 
 const TabLayout = () => {
   return (
@@ -17,8 +16,7 @@ const TabLayout = () => {
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
-            
-            <Feather  name="home" size={20} color={color} />
+            <Feather name="home" size={20} color={color} />
           ),
 
           headerShown: false,
@@ -28,29 +26,29 @@ const TabLayout = () => {
         name="ai-recommendation"
         options={{
           title: "AI Assistant",
-          tabBarIcon: ({ color }) => (
-            
-              <WandSparkles size={20} color={color}/>
-          ),
-          headerShown: false,
+          tabBarIcon: ({ color }) => <WandSparkles size={20} color={color} />,
         }}
       />
       <Tabs.Screen
         name="scan"
         options={{
           title: "Scan QR",
-          tabBarIcon: ({ color }) => (
-            <ScanQrCode size={24} color={color} />
-          ),
+          tabBarButton: (props) => <Pressable {...props} onPress={() => {
+            console.log("Scan QR button pressed");
+          }} />,
+          tabBarIcon: ({ color }) => <ScanQrCode size={20} color={color} />,
           headerShown: false,
         }}
       />
-
       <Tabs.Screen
         name="wardrobe"
         options={{
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="wardrobe-outline" size={24} color={color} />
+            <MaterialCommunityIcons
+              name="wardrobe-outline"
+              size={24}
+              color={color}
+            />
           ),
           headerShown: false,
         }}
